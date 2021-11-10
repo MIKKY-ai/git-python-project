e841edc33207fcede98a9101b69edca4b77e71db
mikky21 <chmikky@gmail.com>
2021-11-10 12:05:22 +0530

    stmt changed

--- main2.py
+++ main2.py
@@ -5,4 +5,5 @@ root = math.sqrt(number)
 if int(root + 0.5) ** 2 == number:
     print(number, "is a perfect square")
 else:
-    print(number, "is not a perfect square")
+    print(numner,"pytohn")
+print(number, "is not a perfect square")
